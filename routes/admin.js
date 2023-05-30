3f5260999b264c2783b5ee495cedc01617c6c0a2
adogbeji <baka_1993@yahoo.co.uk>
2023-05-30 15:52:23 +0100

    Add common starting segment to routes in admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/add-product', (req, res, next) => {
+router.get('/admin/add-product', (req, res, next) => {
     console.log('Middleware 1');
-    res.send('<form action="/product" method="POST"><input type="text" name="name"><button type="submit">Add Product</button></form>');
+    res.send('<form action="/admin/product" method="POST"><input type="text" name="name"><button type="submit">Add Product</button></form>');
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/admin/product', (req, res, next) => {
     console.log(req.body);
     res.redirect('/');
 });
