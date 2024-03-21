46ff8155b2e59aef91e922f0505d9af3b9aa3842
adogbeji <baka_1993@yahoo.co.uk>
2024-03-21 13:18:27 +0000

    Add seocnd middleware & call next() function

--- app.js
+++ app.js
@@ -2,10 +2,17 @@
 
 const express = require('express');
 
-// Next Time: Lesson 60 (Adding Middleware - 2:32)
+// Next Time: Lesson 60 (Adding Middleware - 4:49)
 
 const app = express();
 
-app.use((req, res, next) => {});
+app.use((req, res, next) => {
+    console.log('First Middleware!');
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log('Second Middleware!');
+})
 
 app.listen(3000);
