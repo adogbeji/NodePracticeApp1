2b1dc9cb179b48beb42e85f6a554de6527c1469d
adogbeji <baka_1993@yahoo.co.uk>
2024-04-18 13:38:59 +0100

    Comment out first middleware & define path for second middleware

--- app.js
+++ app.js
@@ -2,16 +2,16 @@
 
 const express = require('express');
 
-// Next Time: Lesson 63 (Handling Different Routes - 0:00)
+// Next Time: Lesson 63 (Handling Different Routes - 1:48)
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('First Middleware!');
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log('First Middleware!');
+//     next();
+// });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log('Second Middleware!');
     res.send('<h1>Hello from Express!</h1>');
 });
